be43162527a23b056885b60c1bcdd8ad33dce89c
CaesarCoder <zhangt4@tcd.ie>
2022-11-02 13:49:54 +0000

    Update House Project.R

--- tutorials/house_price_project/Super Good Answer/House Project.R
+++ tutorials/house_price_project/Super Good Answer/House Project.R
@@ -1,13 +1,5 @@
-House <- read.csv("https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/house_sales.csv")
+House <- read.table("https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/house_sales.csv")
 
-library(tidyverse)
+plot(SalePrice ~ )
 
-House %>%
-  tidyr::pivot_wider(
-    contains("Q7_"),
-    names_to = ("Q7"),
-    values_to = ("programming_language"))
-
-plot()
-
-boxplot() 
+boxplot(SalePrice ~ ) 
