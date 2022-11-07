56f7f6258cf3f410cf74bc6036b1c3cc34086102
CaesarCoder <zhangt4@tcd.ie>
2022-11-07 12:57:48 +0000

    good answer

--- tutorials/house_price_project/Super Good Answer/House Project.R
+++ tutorials/house_price_project/Super Good Answer/House Project.R
@@ -1,5 +1,30 @@
 House <- read.table("https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/house_sales.csv")
 
+saveRDS(dat, "")
+
+ggplot(aes(x = PropertyType, y = SalePrice), # We use the aes() function to supply an x and y argument
+       data = House) + # We use the `+` operator to add an additional geom
+  geom_point(
+
 plot(SalePrice ~ )
 
-boxplot(SalePrice ~ ) 
+
+
+House$SalePriceK <- House$SalePrice / 1000
+
+House$AdjSalePriceK <- House$AdjSalePrice / 1000
+
+
+boxplot(AdjSalePriceK ~ NewConstruction, data = House)
+
+
+boxplot(AdjSalePriceK ~ PropertyType, data = House) 
+
+
+boxplot(AdjSalePriceK ~ Bedrooms, data = House)
+
+
+boxplot(AdjSalePriceK ~ Bathrooms, data = House)
+
+
+boxplot(AdjSalePriceK ~  , data = House)
