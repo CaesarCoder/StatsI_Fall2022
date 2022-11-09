9431da0fa8b1aea29058569e17cb5ecec3cf9cca
CaesarCoder <zhangt4@tcd.ie>
2022-11-09 13:09:49 +0000

    Caesar

--- problemSets/PS03/My_Answer/PS3.R
+++ problemSets/PS03/My_Answer/PS3.R
@@ -1,10 +1,19 @@
 DAT <- read.csv("C:/Users/Caesar/Documents/GitHub/StatsI_Fall2022/datasets/incumbents_subset.csv")
 
+
 ## Q1: 
 LR_VS_DL <- lm(voteshare ~ difflog, data = DAT)
 summary(LR_VS_DL)
 
 ## p-value 2.2e-16, smaller than 0.001, reject the null 
+
+
+library(stargazer)
+output_stargazer <- function(outputFile, ...) {
+  output <- capture.output(stargazer(...))
+  cat(paste(output, collapse = "\n"), "\n", file=outputFile, append=TRUE)
+}
+output_stargazer("regression_output_LR_VS_DL.tex", LR_VS_DL)
 
 
 RS_LR_VS_DL <- residuals(LR_VS_DL)
