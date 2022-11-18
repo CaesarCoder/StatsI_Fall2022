48c84eff248267b66c8a5fc86b1e906fa2763bb8
CaesarCoder <zhangt4@tcd.ie>
2022-11-18 15:13:37 +0000

    Caesar's Answer

--- problemSets/PS03/My_Answer/PS3.R
+++ problemSets/PS03/My_Answer/PS3.R
@@ -8,31 +8,21 @@ library(stargazer)
 LR_VS_DL <- lm(voteshare ~ difflog, data = DAT)
 summary(LR_VS_DL)
 
-## p-value 2.2e-16, smaller than 0.001, reject the null 
+## p-value for the coeeficient of difflog is 2.2e-16, smaller than 0.001, we can
+# reject the null hypothesis that there is no association between voteshare and 
+# difflog statistically significant at the 99.9% level. 
 
-stargazer(LR_VS_DL, title="Regression Results: Vote Share ~ Difflog",
-          type = "LaTex")
+stargazer(LR_VS_DL, title="Regression Results: Vote Share ~ Difflog")
 
-
-
-
-
-
-
-output_stargazer <- function(outputFile, ...) {
-  output <- capture.output(stargazer(...))
-  cat(paste(output, collapse = "\n"), "\n", file=outputFile, append=TRUE)
-}
-output_stargazer("regression_output_LR_VS_DL.tex", LR_VS_DL)
 
 
 RS_LR_VS_DL <- residuals(LR_VS_DL)
 print(RS_LR_VS_DL)
 
-
+png("voteshare ~ difflog.png")
 plot(voteshare ~ difflog, data = DAT)
 abline(LR_VS_DL)
-
+dev.off()
 
 ## prediction equation 
 # y = 0.579 + 0.042x 
@@ -41,20 +31,38 @@ abline(LR_VS_DL)
 
 ## Q2:
 LR_PV_DL <- lm(presvote ~ difflog, data = DAT)
-summary(LR_PV_DL)
+stargazer(LR_PV_DL, title="Regression Results: Presvote ~ Difflog")
 
-## p-value 2.2e-16, smaller than 0.001, reject the null 
+## p-value for the coefficient of difflog is 2.2e-16, smaller than 0.001, we can
+# reject the null hypothesis that there is no association between presvote and 
+# difflog statistically significant at the 99.9% level. 
 
 # prediction equation: 
 # y = 0.508 + 0.024x
 
+png("presvote ~ difflog.png")
+plot(presvote ~ difflog, data = DAT)
+abline(LR_PV_DL)
+dev.off()
+
+
 RS_LR_PV_DL <- residuals(LR_PV_DL)
+print(RS_LR_PV_DL)
 
 ## Q3: 
 LR_VS_PV <- lm(voteshare ~ presvote, data = DAT)
-summary(LR_VS_PV)
+stargazer(LR_VS_PV, title="Regression Results: voteshare ~ Presvote")
 
-## p-value of coefficient for presvote = 2e-16, smaller than 0.001, reject the null 
+## p-value of coefficient for presvote is 2e-16, smaller than 0.001, we can 
+# reject the null hypothesis that there is no association statistically
+# significant between voteshare and presvote at the 99.9% level. 
+
+
+png("voteshare ~ presvote.png")
+plot(voteshare ~ presvote, data = DAT)
+abline(LR_PV_DL)
+dev.off()
+
 
 ## prediction equation: 
 
@@ -63,21 +71,37 @@ summary(LR_VS_PV)
 ## Q4:
 
 LR_RS_PV_DL_VS_DL <- lm(RS_LR_VS_DL ~ RS_LR_PV_DL, data = DAT)
-summary(LR_RS_PV_DL_VS_DL)
+stargazer(LR_RS_PV_DL_VS_DL, title="Regression Results: voteshare ~ Presvote")
 
-# p-value for residual = 2e-16 < 0.001, reject null
+# p-value for the coefficient of residuals from regression model of Q2 is 
+# 2e-16 < 0.001, we can reject the null hypothesis that there is no association
+# statistically significant between residuals from regression model of Q2 and 
+# regression model of Q1 at the 99.9% level.
 
+png("Residual (voteshare ~ difflog) ~ Redisual (presvote ~ difflog).png")
 plot(RS_LR_VS_DL ~ RS_LR_PV_DL, data = DAT)
 abline(LR_RS_PV_DL_VS_DL)
+dev.off()
+
+
 
 ## prediction equation:
 # Y = (2.569e-01)x - 4.860e-18
 
 ## Q5: 
 LR_VS_DL_PV <- lm(voteshare ~ difflog + presvote, data = DAT)
-summary(LR_VS_DL_PV)
-# p-value of the coefficient for difflog = 2e-16 < 0.001, reject null
-# p-value of the coefficient for presvote = 2e-16 < 0.001, reject null 
+stargazer(LR_VS_DL_PV, title = "Regression Results: voteshare ~ difflog + presvote")
+
+# p-value of the coefficient for difflog is 2e-16, smaller than 0.001, we can 
+# reject the null hypothesis that there is no association statistically 
+# significant between vote share and difflog at the 99.9% level.
+
+# p-value of the coefficient for presvote is 2e-16, smaller than 0.001, we can 
+# reject the null  hypothesis that there is no association statistically 
+# significant between vote share and presvote at the 99.9% level.
+
+
+
 # prediction equation:
 # Y = 0.449 + 0.036x1 + 0.257x2
 
@@ -89,3 +113,19 @@ summary(LR_VS_DL_PV)
 # variations by variables voteshare, difflog and presvote. The residuals of Q5 
 # also refers to the unexplained variations by variables voteshare, difflog and 
 # presvote. So the residuals of Q4 and Q5 have the same value. 
+
+
+
+
+
+
+
+## stargazer, define function from PS1 
+
+
+output_stargazer <- function(outputFile, ...) {
+  output <- capture.output(stargazer(...))
+  cat(paste(output, collapse = "\n"), "\n", file=outputFile, append=TRUE)
+}
+output_stargazer("regression_output_LR_VS_DL.tex", LR_VS_DL)
+
